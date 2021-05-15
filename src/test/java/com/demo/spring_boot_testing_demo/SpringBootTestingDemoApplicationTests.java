e3ab7c5c6f3d5f3f281b1acd4295aabe95be8a6e
brecht <brecht.porrez@ordina.be>
2021-05-15 10:52:35 +0200

    initial commit

---
+++ src/test/java/com/demo/spring_boot_testing_demo/SpringBootTestingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.spring_boot_testing_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
